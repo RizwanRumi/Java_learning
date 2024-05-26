563b863d4c4f6776e52f179be306b4f84ef10d3c
DESKTOP-RT8N1K7\Rumi <rizwan.cse33@gmail.com>
2024-05-27 00:52:47 +0200

    add this keyword in setter

--- EncapsulationExample.java
+++ EncapsulationExample.java
@@ -5,22 +5,20 @@
 class  Human {
     private int age;
     private String name;
-
+    
     public int getAge() {
         return age;
     }
-
-    public void setAge(int a) {
-        age = a;
+    public void setAge(int age) {
+        this.age = age;
     }
-
     public String getName() {
         return name;
     }
-    
-    public void setName(String n) {
-        name = n;
+    public void setName(String name) {
+        this.name = name;
     }
+
 }
 
 
