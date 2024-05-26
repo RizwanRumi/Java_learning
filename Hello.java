f93fc905f0e99ffa5f6cb1eb227ed277a37a2089
DESKTOP-RT8N1K7\Rumi <rizwan.cse33@gmail.com>
2024-05-26 16:07:17 +0200

    add basic literals example

---
+++ Hello.java
@@ -0,0 +1,28 @@
+/**
+ * Hello
+ */
+public class Hello {
+
+    public static void main(String[] args) 
+    {
+        // literals
+
+        // int num1 = 0b101; //binary format
+
+        //int num1 =  0x7E; // hexa decimal
+
+        // int num1 = 10_00_00_000; // easy to count zeros
+
+        // double num1 = 12e10;
+
+         //System.out.println(num1);
+
+        char ch = 'a';
+        ch++;
+        System.out.println(ch);
+       
+
+    }  
+      
+}
+
