e3b1c7151b3c07e6c4dfe025a03ec19078e830ae
DESKTOP-RT8N1K7\Rumi <rizwan.cse33@gmail.com>
2024-05-28 16:12:20 +0200

    implement thread using lambda expression

--- ThreadExample.java
+++ ThreadExample.java
@@ -1,32 +1,32 @@
-class A implements Runnable {
-    public void run()
-    {
-        for(int i = 0; i < 5; i++)
-        {
-            System.out.println("Hi");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-}
+// class A implements Runnable {
+//     public void run()
+//     {
+//         for(int i = 0; i < 5; i++)
+//         {
+//             System.out.println("Hi");
+//             try {
+//                 Thread.sleep(10);
+//             } catch (InterruptedException e) {
+//                 e.printStackTrace();
+//             }
+//         }
+//     }
+// }
 
-class B implements Runnable {
-    public void run()
-    {
-        for(int i = 0; i < 5; i++)
-        {
-            System.out.println("Hello");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-}
+// class B implements Runnable {
+//     public void run()
+//     {
+//         for(int i = 0; i < 5; i++)
+//         {
+//             System.out.println("Hello");
+//             try {
+//                 Thread.sleep(10);
+//             } catch (InterruptedException e) {
+//                 e.printStackTrace();
+//             }
+//         }
+//     }
+// }
 
 public class ThreadExample {
     public static void main(String[] args) {
@@ -51,8 +51,33 @@ public class ThreadExample {
         // obj2.start();
 
 
-        Runnable obj1 = new A();
-        Runnable obj2 = new B();
+        // Runnable obj1 = new A();
+        // Runnable obj2 = new B();
+
+        Runnable obj1 = () -> {
+            for(int i = 0; i < 5; i++)
+            {
+                System.out.println("Hi");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Runnable obj2 = () -> {
+            for(int i = 0; i < 5; i++)
+            {
+                System.out.println("Hello");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
 
         Thread t1 = new Thread(obj1);
         Thread t2 = new Thread(obj2);
