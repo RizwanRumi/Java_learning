9e52249dd4d4f4ebd4ba935929ea271ada8031f1
DESKTOP-RT8N1K7\Rumi <rizwan.cse33@gmail.com>
2024-05-28 16:06:24 +0200

    add interface runnable instead of Thread class

--- ThreadExample.java
+++ ThreadExample.java
@@ -1,7 +1,7 @@
-class A extends Thread {
+class A implements Runnable {
     public void run()
     {
-        for(int i = 0; i < 20; i++)
+        for(int i = 0; i < 5; i++)
         {
             System.out.println("Hi");
             try {
@@ -13,10 +13,10 @@ class A extends Thread {
     }
 }
 
-class B extends Thread {
+class B implements Runnable {
     public void run()
     {
-        for(int i = 0; i < 20; i++)
+        for(int i = 0; i < 5; i++)
         {
             System.out.println("Hello");
             try {
@@ -30,24 +30,35 @@ class B extends Thread {
 
 public class ThreadExample {
     public static void main(String[] args) {
-        A obj1 = new A();
-        B obj2 = new B();
+        // A obj1 = new A();
+        // B obj2 = new B();
 
-        System.out.println(obj1.getPriority());
+        // System.out.println(obj1.getPriority());
 
-        /*
-         * SetPriority(): basically you are suggested the schedular the priority, 
-         * then schedular will check who will take the less time, that might be goes first
-         * to execute, SetPriority() does not mean that it will execute always first.  
-         */
-        obj2.setPriority(Thread.MAX_PRIORITY);
+        // /*
+        //  * SetPriority(): basically you are suggested the schedular the priority, 
+        //  * then schedular will check who will take the less time, that might be goes first
+        //  * to execute, SetPriority() does not mean that it will execute always first.  
+        //  */
+        // obj2.setPriority(Thread.MAX_PRIORITY);
 
-        obj1.start();
-        try {
-            Thread.sleep(2);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        obj2.start();
+        // obj1.start();
+        // try {
+        //     Thread.sleep(2);
+        // } catch (InterruptedException e) {
+        //     e.printStackTrace();
+        // }
+        // obj2.start();
+
+
+        Runnable obj1 = new A();
+        Runnable obj2 = new B();
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
+        t1.start();
+        t2.start();    
+
     }
 }
