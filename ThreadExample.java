454e4b88b3221b1c8dce2ca88a41e67fea8a8d11
DESKTOP-RT8N1K7\Rumi <rizwan.cse33@gmail.com>
2024-05-28 15:57:07 +0200

    add thread priority and sleep example

--- ThreadExample.java
+++ ThreadExample.java
@@ -1,9 +1,14 @@
 class A extends Thread {
     public void run()
     {
-        for(int i = 0; i < 100; i++)
+        for(int i = 0; i < 20; i++)
         {
             System.out.println("Hi");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -11,9 +16,14 @@ class A extends Thread {
 class B extends Thread {
     public void run()
     {
-        for(int i = 0; i < 100; i++)
+        for(int i = 0; i < 20; i++)
         {
             System.out.println("Hello");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -23,7 +33,21 @@ public class ThreadExample {
         A obj1 = new A();
         B obj2 = new B();
 
+        System.out.println(obj1.getPriority());
+
+        /*
+         * SetPriority(): basically you are suggested the schedular the priority, 
+         * then schedular will check who will take the less time, that might be goes first
+         * to execute, SetPriority() does not mean that it will execute always first.  
+         */
+        obj2.setPriority(Thread.MAX_PRIORITY);
+
         obj1.start();
+        try {
+            Thread.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         obj2.start();
     }
 }
