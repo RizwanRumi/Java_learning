d71c553f33092ebeab15514d17bb7ec1fd078588
DESKTOP-RT8N1K7\Rumi <rizwan.cse33@gmail.com>
2024-06-09 22:51:33 +0200

    add multithreading example using Thread class in a same class

--- MultiThreading/ThreadTest.java
+++ MultiThreading/ThreadTest.java
@@ -3,7 +3,7 @@ package MultiThreading;
 
 // use Thread class 
 
-
+/* 
 class MyThreadTest extends Thread
 {
 
@@ -22,6 +22,34 @@ public class ThreadTest
     public static void main(String[] args) {
 
         MyThreadTest t = new MyThreadTest();
+        t.start();
+
+        int i=1;
+        while (true) {
+            System.out.println(i + " world");
+            i++;
+        }
+        
+    }
+}
+
+*/
+
+public class ThreadTest extends Thread
+{
+    @Override
+    public void run() {
+        int i=1;
+        while (true) {
+            System.out.println(i + " Hello");
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ThreadTest m = new ThreadTest();
+        Thread t = new Thread(m);
         t.start();
 
         int i=1;
