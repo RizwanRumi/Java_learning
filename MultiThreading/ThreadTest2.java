3b5e1e5bb86efb369d9eb4e6aa68fc90194a83a2
DESKTOP-RT8N1K7\Rumi <rizwan.cse33@gmail.com>
2024-06-09 22:59:28 +0200

    add multithreading example using Runnable interface in a same class

--- MultiThreading/ThreadTest2.java
+++ MultiThreading/ThreadTest2.java
@@ -2,7 +2,7 @@ package MultiThreading;
 
 // use Runnable interface 
 
-
+/*
 class MyThreadTest implements Runnable
 {
 
@@ -21,6 +21,36 @@ public class ThreadTest2
     public static void main(String[] args) {
 
         MyThreadTest m = new MyThreadTest();
+
+        Thread t = new Thread(m);
+        t.start();
+
+        int i=1;
+        while (true) {
+            System.out.println(i + " world");
+            i++;
+        }
+        
+    }
+}
+
+*/
+
+
+public class ThreadTest2 implements Runnable
+{
+    @Override
+    public void run() {
+        int i=1;
+        while (true) {
+            System.out.println(i + " Hello");
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ThreadTest2 m = new ThreadTest2();
 
         Thread t = new Thread(m);
         t.start();
